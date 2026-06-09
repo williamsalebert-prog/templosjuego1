eb380556fc21e81640fd86568f776fcd53c959e6
williamsalebert-prog <williamsalebert@gmail.com>
2026-06-09 07:37:42 -0600

    Update f4.js

--- js/f4.js
+++ js/f4.js
@@ -19,14 +19,12 @@ class F4 extends Pieza {
             let c = col + dc;
             while (f >= 0 && f < FILAS && c >= 0 && c < COLUMNAS && esJugable(f, c)) {
                 if (board[f][c] === null) {
-                    // Casilla vacía: movimiento normal
                     let clave = `${f},${c}`;
                     destinos.add(clave);
                     if (!caminos[clave]) caminos[clave] = [{ tipo: 'move', to: [f, c] }];
                 } else {
                     let piezaEncontrada = board[f][c];
                     if (piezaEncontrada.jugador === jugador) {
-                        // Pieza amiga: puede saltarla y ocupar la casilla detrás
                         let detrasF = f + df, detrasC = c + dc;
                         if (detrasF >= 0 && detrasF < FILAS && detrasC >= 0 && detrasC < COLUMNAS &&
                             board[detrasF][detrasC] === null && esJugable(detrasF, detrasC)) {
@@ -39,7 +37,6 @@ class F4 extends Pieza {
                             }];
                         }
                     }
-                    // Si es enemiga, el Trampero se detiene sin saltar
                     break;
                 }
                 f += df;
