e72fb4c1f575c612a83c46f51e250d5fcbf1943d
williamsalebert-prog <williamsalebert@gmail.com>
2026-06-09 07:37:59 -0600

    Update f5.js

--- js/f5.js
+++ js/f5.js
@@ -7,7 +7,6 @@ class F5 extends Pieza {
 
     obtenerMovimientos(fila, col, board) {
         const jugador = this.jugador;
-        // Solo diagonales
         const dirs = [[-1,-1], [-1,1], [1,-1], [1,1]];
         let destinos = new Set();
         let caminos = {};
@@ -17,27 +16,25 @@ class F5 extends Pieza {
             let c = col + dc;
             while (f >= 0 && f < FILAS && c >= 0 && c < COLUMNAS && esJugable(f, c)) {
                 if (board[f][c] === null) {
-                    // Casilla vacía
                     let clave = `${f},${c}`;
                     destinos.add(clave);
                     if (!caminos[clave]) caminos[clave] = [{ tipo: 'move', to: [f, c] }];
                 } else {
                     let piezaEncontrada = board[f][c];
                     if (piezaEncontrada.jugador !== jugador) {
-                        // Enemiga: salto o captura directa
                         let detrasF = f + df, detrasC = c + dc;
                         if (detrasF >= 0 && detrasF < FILAS && detrasC >= 0 && detrasC < COLUMNAS &&
-                            board[detrasF][detrasC] === null && esJugable(detrasF, detrasC)) {
-                            // Salto normal
-                            let clave = `${detrasF},${detrasC}`;
-                            destinos.add(clave);
-                            if (!caminos[clave]) caminos[clave] = [{
-                                tipo: 'jump',
-                                over: [f, c],
-                                to: [detrasF, detrasC]
-                            }];
+                            esJugable(detrasF, detrasC)) {
+                            if (board[detrasF][detrasC] === null) {
+                                let clave = `${detrasF},${detrasC}`;
+                                destinos.add(clave);
+                                if (!caminos[clave]) caminos[clave] = [{
+                                    tipo: 'jump',
+                                    over: [f, c],
+                                    to: [detrasF, detrasC]
+                                }];
+                            }
                         } else {
-                            // Extremo: captura directa
                             let clave = `${f},${c}`;
                             destinos.add(clave);
                             if (!caminos[clave]) caminos[clave] = [{
@@ -47,7 +44,6 @@ class F5 extends Pieza {
                             }];
                         }
                     }
-                    // El alfil se detiene al encontrar cualquier pieza
                     break;
                 }
                 f += df;
