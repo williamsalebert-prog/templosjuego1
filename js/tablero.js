1a7266f7b28ed7ae9b3cf000cafbc5bb71c23fbc
williamsalebert-prog <williamsalebert@gmail.com>
2026-06-09 07:39:11 -0600

    Update tablero.js

--- js/tablero.js
+++ js/tablero.js
@@ -156,7 +156,6 @@ function aplicarMovimiento(origen, destino) {
     } else if (paso.tipo === 'captureDirect') {
         let [of, oc] = paso.over;
         let [nf, nc] = paso.to;
-        // Capturar la pieza enemiga en el destino
         let piezaObjetivo = board[of][oc];
         if (piezaObjetivo && piezaObjetivo.jugador !== jugador) {
             if (piezaObjetivo.tipo !== 'F4' || pieza.tipo === 'F3' || pieza.tipo === 'F6') {
@@ -170,33 +169,18 @@ function aplicarMovimiento(origen, destino) {
     } else if (paso.tipo === 'jump') {
         let [of, oc] = paso.over;
         let [nf, nc] = paso.to;
-        if (of === nf && oc === nc) {
-            // Caso especial (ya no debería ocurrir con el nuevo sistema)
-            let piezaSaltada = board[of][oc];
-            if (piezaSaltada && piezaSaltada.jugador !== jugador) {
-                if (piezaSaltada.tipo !== 'F4' || pieza.tipo === 'F3' || pieza.tipo === 'F6') {
-                    carcela.agregar(piezaSaltada);
-                    board[of][oc] = null;
-                }
+        let piezaSaltada = board[of][oc];
+        if (piezaSaltada && piezaSaltada.jugador !== jugador) {
+            if (piezaSaltada.tipo === 'F4' && pieza.tipo !== 'F3' && pieza.tipo !== 'F6') {
+                // no se captura
+            } else {
+                carcela.agregar(piezaSaltada);
+                board[of][oc] = null;
             }
-            board[f][c] = null;
-            board[nf][nc] = pieza;
-            f = nf; c = nc;
-        } else {
-            // Salto normal
-            let piezaSaltada = board[of][oc];
-            if (piezaSaltada && piezaSaltada.jugador !== jugador) {
-                if (piezaSaltada.tipo === 'F4' && pieza.tipo !== 'F3' && pieza.tipo !== 'F6') {
-                    // no se captura
-                } else {
-                    carcela.agregar(piezaSaltada);
-                    board[of][oc] = null;
-                }
-            }
-            board[f][c] = null;
-            board[nf][nc] = pieza;
-            f = nf; c = nc;
         }
+        board[f][c] = null;
+        board[nf][nc] = pieza;
+        f = nf; c = nc;
     }
 }
 
