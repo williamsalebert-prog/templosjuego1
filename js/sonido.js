50f878026539c5aca06c053e8f03d16d78f93068
williamsalebert-prog <williamsalebert@gmail.com>
2026-06-21 08:59:01 -0600

    Update sonido.js

--- js/sonido.js
+++ js/sonido.js
@@ -23,3 +23,19 @@ function playTone(frec, dur, tipo = 'triangle', vol = 0.1) {
 function sonidoMovimiento() { playTone(300, 0.1, 'triangle', 0.08); }
 function sonidoSalto() { playTone(300, 0.1, 'triangle', 0.08); }
 function sonidoEnroque() { playTone(400, 0.2, 'sine', 0.12); playTone(600, 0.2, 'sine', 0.12); }
+
+// Aviso corto de jaque (dos tonos cortos y agudos)
+function sonidoJaque() {
+    playTone(880, 0.12, 'square', 0.1);
+    setTimeout(() => playTone(988, 0.15, 'square', 0.1), 130);
+}
+
+// Pequeño "brillo" ascendente para cuando un peón va a coronar
+function sonidoCoronacion() {
+    playTone(523, 0.1, 'sine', 0.1);
+    setTimeout(() => playTone(659, 0.1, 'sine', 0.1), 90);
+    setTimeout(() => playTone(784, 0.18, 'sine', 0.1), 180);
+}
+
+// Aviso de poco tiempo en el reloj
+function sonidoTiempoBajo() { playTone(220, 0.18, 'sawtooth', 0.08); }
